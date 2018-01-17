39285307750c1d6c5ced4ff561b48bf9d63cc076
inleidingJava <peyman_mah6@yahoo.com>
2018-01-17 09:37:31 +0100

    Eerste java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
-public class Main {
+import java.applet.Applet;
+import java.awt.*;
+import java.text.AttributedCharacterIterator;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Main extends Applet {
+
+    public void init() {
+
+        setSize(400, 300);
+        setBackground(Color.white);
     }
+    public  void paint(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.drawString("Peyman", 50, 60);
+        g.setColor(Color.RED);
+        g.drawString("Mahmoudi", 50, 80);
+    }
+
 }
